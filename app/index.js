0b183d9db5ab73882af328397142ce686e2f43a0
userjapa <willian.hsimabukuro@gmail.com>
2022-03-22 00:15:36 -0300

    stoped at creating transaction pool tests

---
+++ app/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+const Blockchain = require('../blockchain')
+
+const P2pServer = require('./p2p-server')
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001
+
+const app = express()
+
+const bc = new Blockchain()
+
+const p2pServer = new P2pServer(bc)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/blocks', (req, res) => {
+  res.json(bc.chain)
+})
+
+app.post('/mine', (req, res) => {
+  const block = bc.addBlock(req.body.data)
+  console.log(`New block added: ${block.toString()}`)
+
+  p2pServer.syncChains()
+
+  res.redirect('/blocks')
+})
+
+app.listen(HTTP_PORT, () => {
+  console.log(`Listening at port ${HTTP_PORT}`)
+})
+
+p2pServer.listen()
---
+++ config.js
@@ -0,0 +1,9 @@
+const DIFFICULTY      = 4, // Length
+      MINE_RATE       = 3000, // Milliseconds
+      INITIAL_BALANCE = 500
+
+module.exports = {
+  DIFFICULTY,
+  MINE_RATE,
+  INITIAL_BALANCE
+}
