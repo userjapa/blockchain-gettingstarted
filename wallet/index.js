b815c73cfef2d2a7f05b14dbda9570472d29f591
userjapa <willian.hsimabukuro@gmail.com>
2022-03-22 00:37:03 -0300

    added wallet tests

--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,5 @@
+const Transaction = require('./transaction')
+
 const ChainUtil = require('../chain-util')
 
 const { INITIAL_BALANCE } = require('../config')
@@ -18,6 +20,24 @@ class Wallet {
 
   sign (dataHash) {
     return this.keyPair.sign(dataHash)
+  }
+
+  createTransaction (recipient, amount, transactionPool) {
+    if (amount > this.balance) {
+      console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`)
+      return
+    }
+
+    let transaction = transactionPool.existingTransaction(this.publicKey)
+
+    if (transaction) {
+      transaction.update(this, recipient, amount)
+    } else {
+      transaction = Transaction.newTransaction(this, recipient, amount)
+      transactionPool.updateOrAddTransaction(transaction)
+    }
+
+    return transaction
   }
 }
 
